43e151ecb6244c1ad2fbc7767ddd9c825208065e
Chii <chibuokem_tolu@hotmail.com>
2019-07-09 00:13:48 +0100

    feature(trip): Admin can create trips [Start #167152382]

--- src/db.sql
+++ src/db.sql
@@ -9,3 +9,25 @@ CREATE TABLE IF NOT EXISTS users (
   password VARCHAR NOT NULL,
   is_admin BOOLEAN NOT NULL DEFAULT FALSE
 );
+
+CREATE TABLE IF NOT EXISTS buses (
+  id SERIAL PRIMARY KEY,
+  number_plate VARCHAR NOT NULL UNIQUE,
+  manufacturer VARCHAR NOT NULL,
+  model VARCHAR NOT NULL, 
+  year VARCHAR NOT NULL,
+  capacity INTEGER NOT NULL,
+  created_by SERIAL NOT NULL
+);
+
+CREATE TYPE status AS ENUM ('active', 'cancelled');
+CREATE TABLE IF NOT EXISTS trips (
+  id SERIAL PRIMARY KEY,
+  bus_id SERIAL NOT NULL,
+  origin VARCHAR NOT NULL,
+  destination VARCHAR NOT NULL,
+  trip_date DATE NOT NULL, 
+  fare INTEGER NOT NULL,
+  status status NOT NULL DEFAULT 'active',
+  created_by SERIAL NOT NULL
+);
