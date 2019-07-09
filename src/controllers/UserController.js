9fd53b1de63ac060afa0143533c81afd0414a94c
Chii <chibuokem_tolu@hotmail.com>
2019-07-09 14:18:36 +0100

    bug(setup): fix bug in travis ci

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -2,7 +2,7 @@
 import Model from '../models/Model';
 import returnError from '../helpers/errorHandler';
 
-const Users = new Model('users');
+const Users = new Model('wayfarer.users');
 
 export default class UserController {
   static async signIn(req, res) {
