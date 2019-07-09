6a91d1698fab3882db5616eb3423e9a3ee79b926
Chii <chibuokem_tolu@hotmail.com>
2019-07-09 14:23:13 +0100

    bug(setup): Fix bug in travis ci setup

--- src/middlewares/UserMiddleware.js
+++ src/middlewares/UserMiddleware.js
@@ -2,7 +2,7 @@
 import Model from '../models/Model';
 import returnError from '../helpers/errorHandler';
 
-const Users = new Model('wayfarer.users');
+const Users = new Model('"wayfarer"."users"');
 
 export default class UserMiddleware {
   static async validateUserData(req, res, next) {
